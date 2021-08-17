2740bf7bebf7e7943c6bda03ec115177fa17bbb2
Jaroslaw Lada <lada.jaroslaw@gmail.com>
2021-08-17 19:09:05 +0200

    Add Employee

---
+++ src/main/java/pl/sda/zadanie1/Employee.java
@@ -0,0 +1,27 @@
+package pl.sda.zadanie1;
+
+public class Employee  extends Person {
+    private String job;
+    private double salary;
+
+    public Employee(String job, double salary) {
+        this.job = job;
+        this.salary = salary;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
