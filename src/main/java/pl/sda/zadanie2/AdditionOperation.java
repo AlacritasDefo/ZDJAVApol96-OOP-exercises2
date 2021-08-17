01da86978a9c41c0658014e715f4bf36f11da402
AlacritasDefo <arkadiuszprzychocki@gmail.com>
2021-08-17 20:06:49 +0200

    zadanie2 rozwiązanie

--- src/main/java/pl/sda/zadanie2/AdditionOperation.java
+++ src/main/java/pl/sda/zadanie2/AdditionOperation.java
@@ -3,6 +3,7 @@ package pl.sda.zadanie2;
 public class AdditionOperation implements Operation {
 
 
+<<<<<<< HEAD
 
     @Override
     public int calculate(int a, int b) {
@@ -10,3 +11,10 @@ public class AdditionOperation implements Operation {
     }
 }
 
+=======
+    @Override
+    public int calculate(int a, int b) {
+        return a+b;
+    }
+}
+>>>>>>> origin/master
--- src/main/java/pl/sda/zadanie2/Operation.java
+++ src/main/java/pl/sda/zadanie2/Operation.java
@@ -1,6 +1,11 @@
 package pl.sda.zadanie2;
 
 public interface Operation {
+<<<<<<< HEAD
     int calculate (int a, int b);
 
+=======
+
+    int calculate (int a, int b);
+>>>>>>> origin/master
 }
